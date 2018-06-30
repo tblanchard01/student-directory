dcfe19bc3eaf83c568da709589c07742d0c1831c
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-06-30 14:12:32 +0000

    added user input to student dir

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
 #all students into hashes 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  #get first name
+  name = gets.chomp
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name 
+    name = gets.chomp
+  end 
+  #return students
+  students
+end 
 
 def print_header
   puts "The students of Villains Academy"
@@ -23,13 +27,12 @@ def print(names)
   names.each do |name| puts "#{name[:name]} (#{name[:cohort]} cohort)" end 
 end 
 
-
 def print_footer(names)
   #prints the total number of students
   #it’s important that print() doesn’t add new line characters
   puts "Overall, we have #{names.count} great students"
 end 
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
