9ba1a8858bd61ed9e85f68df6c6ec23904166bbd
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-06-30 10:59:37 +0000

    added iteration of students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,25 @@
+student_count = 11
+#all students into array 
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-#first we print the list of students
+#prints out students in a list. 
+
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have "
+students.each do |student| puts student end 
+
+puts "Overall, we have #{student_count} great students"
 # finally, we print the total number of students
-print 11
-puts " great students"
 #it’s important that print() doesn’t add new line characters
