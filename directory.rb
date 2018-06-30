4f7e6adf421e03729b2f0b3a2247861f97730b4e
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-06-30 12:31:45 +0000

    coverted students dir to hash

--- directory.rb
+++ directory.rb
@@ -1,17 +1,16 @@
-student_count = 11
-#all students into array 
+#all students into hashes 
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -21,8 +20,9 @@ end
 
 def print(names)
   #prints out students in a list. 
-  names.each do |name| puts name end 
+  names.each do |name| puts "#{name[:name]} (#{name[:cohort]} cohort)" end 
 end 
+
 
 def print_footer(names)
   #prints the total number of students
