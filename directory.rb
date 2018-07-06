0571e5a7cdfe5b6217983d5ba770ebd21812f007
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-06 10:10:25 +0000

    added exporting to students.csv functionality

--- directory.rb
+++ directory.rb
@@ -37,10 +37,23 @@ def print_footer
   #it’s important that print() doesn’t add new line characters
   @students.count == 1? (puts "We currently have 1 great student") : (puts "We currently have #{@students.count} students")
 end 
+
+def save_students 
+  file = File.open("students.csv", "w")
+  @students.each do |student| 
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+             end 
+  file.close 
+
+end 
+
 def print_menu
 #print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end 
 
@@ -50,6 +63,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
